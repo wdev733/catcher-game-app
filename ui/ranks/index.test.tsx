e3e814dfba6ea94aa347d064c17e27fe96c50720
wdev733 <wdev733@gmail.com>
2023-08-04 16:38:43 -0400

    add simple test case

---
+++ ui/ranks/index.test.tsx
@@ -0,0 +1,13 @@
+import { act } from 'react-dom/test-utils';
+
+import { screen, render } from 'test.utils';
+import RankContainer from 'ui/ranks';
+
+describe('Top 100 Rankers View', () => {
+  test('Should render title', async () => {
+    await act(() => {
+      render(<RankContainer />);
+    });
+    expect(screen.getByText('Top Rankers')).toBeInTheDocument();
+  });
+});
