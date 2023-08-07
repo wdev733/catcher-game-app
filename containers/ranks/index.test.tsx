d34e1c3668f900890f444ded9d14aceab9419a86
wdev733 <wdev733@gmail.com>
2023-08-07 05:19:30 -0400

    add mock router to test

--- containers/ranks/index.test.tsx
+++ containers/ranks/index.test.tsx
@@ -1,7 +1,18 @@
 import { act } from 'react-dom/test-utils';
 
 import RankContainer from 'containers/ranks';
+import * as nextRouter from 'next/router';
 import { screen, render } from 'test.utils';
+import { instance, mock } from 'ts-mockito';
+
+const push = jest.fn();
+const mockedRouterType = mock<nextRouter.NextRouter>({});
+const mockedRouter = instance(mockedRouterType);
+jest.spyOn(nextRouter, 'useRouter').mockImplementation(() => ({
+  ...mockedRouter,
+  route: '/',
+  push,
+}));
 
 describe('Top 100 Rankers View', () => {
   test('Should render title', async () => {
